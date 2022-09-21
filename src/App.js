e34be7ea4ea2a1b5d208303cd185d7f17f84aa55
nataliey16 <102062208+nataliey16@users.noreply.github.com>
2022-09-21 12:07:41 -0600

    Added boostrap

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather App</h1>
+        </header>
+        <footer>
+          <a
+            href="https://github.com/nataliey16/search-engine-react.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source code
+          </a>{" "}
+          by Natalie Yeung
+        </footer>
+      </div>
     </div>
   );
 }
